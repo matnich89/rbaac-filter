8821e904af8bf82873231b3276d5e153e05c8858
Mathew Nicholls <mathew.nicholls@imgarena.com>
2022-04-09 19:22:48 +0100

    add logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,14 @@ import (
 )
 
 func main() {
+	log.Println("starting.....")
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		log.Fatalln("This application has to be ran instead a k8s cluster...")
 	}
 	_, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("issue getting new config %s", err.Error())
+	}
+	log.Println("started....")
 }
